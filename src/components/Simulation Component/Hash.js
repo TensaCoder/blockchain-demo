08a0a6e864a04dec1104af15ed58bd564b95d1f3
TensaCoder <herschel.menezes00@gmail.com>
2023-01-13 00:12:06 +0530

    hash component

---
+++ src/components/Simulation Component/Hash.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import '../../CSS/hash.css'
+
+const Hash = () => {
+
+    const [hashValue, setHashValue] = useState("")
+    const [dataInput, setDataInput] = useState("")
+
+
+    return (
+        <>
+            <div className='container mx-auto'>
+                <h2 className='mt-4 width-setting'>Hash</h2>
+
+
+                <form className='mt-4 container width-setting'>
+                    <div className="mb-3">
+                        <div className='hash-block'>
+                        <label htmlFor="privateKey" className="form-label">Data</label>
+                        <input type="text" className="form-control hashInput" id="privateKey" value={dataInput}/>
+
+                        </div>
+                        {/* <div className="form-text">Never share your Private Key with anyone else.</div> */}
+                    </div>
+
+                    <div className="mb-3 mt-4">
+                        <div className='hash-block'>
+                        <label htmlFor="publicKey" className="form-label">Hash Value</label>
+                        <input type="text" className="form-control" id="publicKey" value={hashValue} />
+                        </div>
+
+                        {/* <div className="form-text">Public Key will be available to everyone.</div> */}
+                    </div>
+
+
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Hash
