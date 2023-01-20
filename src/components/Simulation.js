7bcc60d39f271590e2112dbe0a871703a55109e3
TensaCoder <herschel.menezes00@gmail.com>
2023-01-20 22:08:24 +0530

    Simulation file modified

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Asymmetric from "./Simulation Component/Asymmetric"
+import Block from './Simulation Component/Block'
 import Hash from './Simulation Component/Hash'
 import SimNavbar from './Simulation Component/SimNavbar'
 
+import IndividualBlock from './Simulation Component/additionalComponents/IndividualBlock'
+import Blockchain from './Simulation Component/Blockchain'
+import DistributedBlockchain from './Simulation Component/DistributedBlockchain'
+
 const Simulation = () => {
+
+
     return (
         <>
             {/* <div className='d-flex'>
@@ -14,7 +21,14 @@ const Simulation = () => {
 
             
             {/* <Asymmetric /> */}
-            <Hash />
+            {/* <Hash /> */}
+            {/* <Block /> */}
+            
+
+            {/* <Blockchain /> */}
+            {/* <IndividualBlock /> */}
+
+            <DistributedBlockchain />
         </>
     )
 }
